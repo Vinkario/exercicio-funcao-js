cdf8f37030d08e1fc4b97d4d10ee68f6c62765b3
Vinkario <gadblake66@gmail.com>
2022-08-16 15:07:56 -0300

    ex03

---
+++ ex03/index.js
@@ -0,0 +1,14 @@
+//3 – Peça ao usuário para digitar 5 números em uma caixa de texto. Verifique qual é o maior número e exiba-o.
+function numeroMaior(num1,num2,num3,num4,num5){
+    return maiorNumero
+
+}
+
+const num1 = Number(prompt('Digite o primeiro numero'));
+const num2 = Number(prompt('Digite o segundo numero'));
+const num3 = Number(prompt('Digite o terceiro numero'));
+const num4 = Number(prompt('Digite o quarto numero'));
+const num5 = Number(prompt('Digite o quinto numero'));
+const maiorNumero = Math.max(num1,num2,num3,num4,num5);
+
+document.write('O maior numero digitado é: ',maiorNumero);
